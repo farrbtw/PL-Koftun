fd5fcd7cd1d87152b04a789bd8d2336bd7ff7325
farrbtw <farrpatt888@gmail.com>
2025-04-08 01:36:37 +0300

    Create 1задание.java

---
+++ 1задание.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        String first = scanner.nextLine();
+        String second = scanner.nextLine();
+
+        boolean endsWith = first.endsWith(second);
+
+        System.out.println(endsWith);
+        
+        scanner.close();
+    }
+}
+
+//1 задание
